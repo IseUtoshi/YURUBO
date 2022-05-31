d8b46036b43e88346817989ad98f715e7f81f337
IseUtoshi <sugar.buseinss@gmail.com>
2022-05-31 19:29:22 +0900

    フォームの表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   resources :users, only: :show
-  resources :posts, only: [:show, :new]
+  resources :posts, only: [:show, :new, :create]
 end
