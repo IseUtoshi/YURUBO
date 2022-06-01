324a2447a79af78070580d937f3001e23a9907fe
IseUtoshi <sugar.buseinss@gmail.com>
2022-06-01 17:56:00 +0900

    ゲームタイトルの保存

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,15 @@
 class GamesController < ApplicationController
   def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to root_path
+    else
+      render "users/show"
+    end
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:title).merge(user_id: current_user.id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def show
+    @game = Game.new
   end
-  
+
 end
