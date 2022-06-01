c8e7197f7f8cf7ad02498269eff5cd23c88c7d7d
IseUtoshi <sugar.buseinss@gmail.com>
2022-06-01 19:39:18 +0900

    バリデーションの追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,4 +13,10 @@ class Post < ApplicationRecord
   validates :publish_id, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :user_id, presence: true
 
+  validate :time_mismatch
+
+  def time_mismatch
+    errors.add(:end_time, "終了時刻が開始時刻の前です") if end_time < start_time
+  end
+
 end
