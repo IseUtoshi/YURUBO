2ef3c3e294570c5617058729d5b9baac356a3994
IseUtoshi <sugar.buseinss@gmail.com>
2022-06-01 16:08:39 +0900

    バリデーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,10 @@ class Post < ApplicationRecord
   belongs_to :chat
   belongs_to :publish
 
+  validates :start_time, presence: true
+  validates :end_time, presence: true
   validates :chat_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :publish_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :user_id, presence: true
 
 end
--- app/models/post_game.rb
+++ app/models/post_game.rb
@@ -1,4 +1,8 @@
 class PostGame < ApplicationRecord
   belongs_to :post
   belongs_to :game
+
+  validates :post_id, presence: true
+  validates :game_id, presence: true
+
 end
