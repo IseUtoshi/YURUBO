eb24af52c236d33c21b4ebe98e95163035f35b11
IseUtoshi <sugar.buseinss@gmail.com>
2022-06-02 14:12:20 +0900

    日付を反映する操作

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,26 @@ class PostsController < ApplicationController
   end
 
   def new
+    @day = params[:date]
     @post = Post.new
     @games = Game.where(user_id: "1").or(Game.where(user_id: current_user.id))
   end
 
   def create
+    @post = Post.new(post_params)
+    @games = Game.where(user_id: "1").or(Game.where(user_id: current_user.id))
+    if @post.save
+      redirect_to root_path
+    else
+      @day = "#{params[:post]["start_time(1i)"]}-#{params[:post]["start_time(2i)"]}-#{params[:post]["start_time(3i)"]}"
+      redirect_to new_post_path(date: @day)
+    end
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:start_time, :end_time, :chat_id, :publish_id, game_ids: []).merge(user_id: current_user.id)
+  end
+
 end
