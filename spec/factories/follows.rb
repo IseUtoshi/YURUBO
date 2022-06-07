59dc4a548326ee53aceed760c465b91cee0543cc
IseUtoshi <sugar.buseinss@gmail.com>
2022-06-07 20:00:47 +0900

    テストコードの作成

--- spec/factories/follows.rb
+++ spec/factories/follows.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :follow do
-    follower_id { Faker::Number.within(range: 1..10) }
-    followee_id { Faker::Number.within(range: 11..20) }
+    
   end
 end
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Follow, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @another_user = FactoryBot.create(:user)
+    @follow = @user.follow(@another_user.id)
+  end
+
+  describe 'フォローする' do
+    context 'フォローできる場合' do
+      it '正しくフォロー' do
+        expect(@follow).to be_valid
+      end
+    end
+    context 'フォローできない場合' do
+      it 'フォロワーが空' do
+        @follow.follower_id = ''
+        @follow.valid?
+        expect(@follow.errors.full_messages).to include("Follower must exist")
+      end
+      it 'フォロイーが空' do
+        @follow.followee_id = ''
+        @follow.valid?
+        expect(@follow.errors.full_messages).to include("Followee must exist")
+      end
+      it '既にフォロー済み' do
+        @follow.save
+        another_follow = @user.follow(@another_user.id)
+        another_follow.follower_id = @follow.follower_id
+        another_follow.followee_id = @follow.followee_id
+        another_follow.valid?
+        expect(another_follow.errors.full_messages).to include("Follower has already been taken")
+      end
+      it '自身をフォロー' do
+        @follow.followee_id = @follow.follower_id
+        @follow.valid?
+        expect(@follow.errors.full_messages).to include("フォロー対象が自分自身です")
+      end
+    end
+  end
 end
