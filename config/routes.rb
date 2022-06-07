3446839f96fba4156971b4169f3c3577b129f839
IseUtoshi <sugar.buseinss@gmail.com>
2022-06-07 16:51:47 +0900

    ルーティングとコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-  resources :users, only: :show
+  resources :users, only: :show do
+    member do
+      get :follows, :followers
+    end
+    resources :follows, only: [:create, :destroy]
+  end
   resources :posts, only: [:show, :new, :create]
   resources :games, only: :create
 end
