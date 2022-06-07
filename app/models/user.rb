7042999c9bab5453da1d2304efd7f0e2ab131c0b
IseUtoshi <sugar.buseinss@gmail.com>
2022-06-07 16:41:08 +0900

    アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :games
+  has_many :follower, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :followee, class_name: "Follow", foreign_key: "followee_id", dependent: :destroy
+  has_many :followee_user, through: :follower, source: :followee
+  has_many :follower_user, through: :followee, source: :follower
 end
